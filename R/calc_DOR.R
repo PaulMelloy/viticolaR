bcabc317c326e8da37fcdce5881c0f94863f3d37
PaulMelloy <paul@melloy.com.au>
2023-09-02 06:22:06 +1000

    add Ro header

--- R/calc_DOR.R
+++ R/calc_DOR.R
@@ -1,4 +1,14 @@
-calc_DOR <- function(hh, Tm, vpd, rain){
+#' Title
+#'
+#' @param Tm
+#' @param vpd
+#' @param rain
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_DOR <- function(Tm_h, vpd_h, rain_h){
 
   if(rain > 0 | vpd == 4.5){
     M_h <- 1
@@ -8,15 +18,9 @@ calc_DOR <- function(hh, Tm, vpd, rain){
     }
   }
 
-  # The HT (hydrothermal time) is also used to calculate the length of the primary
-  #  inoculum season (PIS): PIS starts and finishes when HT = 1.3 and 8.6,
-  #  respectively (Fig. 3).
-  # These values correspond to the time when 3% and 97% of the total oospores
-  #  forming the SOD have entered the PMO stage, respectively, according to Eq. (3)
 
 
-  #Calculate Hydrothermal time
-  HT_h <- M_h/(1330.1 - 116.19 * T_h + 2.6256 * (T_h^2))
+
 
   DOR_h <- exp(-15.891*exp(-0.653*(HT_h + 1)))
 }
