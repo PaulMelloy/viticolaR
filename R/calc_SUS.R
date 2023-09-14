af52be261b97adac8264a49cf7d4165323bdc345
PaulMelloy <paul@melloy.com.au>
2023-09-15 06:46:21 +1000

    Add Roxy doc

--- R/calc_SUS.R
+++ R/calc_SUS.R
@@ -1,3 +1,13 @@
+#' Calculate sporangia survival
+#'
+#' @param T_h numeric, hourly temperature mean
+#' @param RH_h numeric, hourly relative humidity mean
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' calc_SUS(25,95)
 calc_SUS <- function(T_h,RH_h){
   SUS <- 1/(
     24*(5.67 * 0.47 *
