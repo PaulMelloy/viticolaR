139fcbdfd4b0c2d7bb2be1dadbcd24d5b1f54c4e
PaulMelloy <paul@melloy.com.au>
2023-09-15 06:55:42 +1000

    Try to correct function. but probs not needed

--- R/calc_DOR.R
+++ R/calc_DOR.R
@@ -1,21 +1,26 @@
 #' Calculate leaf litter Moisture
 #'
 #' @param Tm numeric, hourly mean Temperature in degrees
-#' @param vpd numeric, hourly mean vapour pressure defiecit in hPa
+#' @param vpd numeric, hourly mean vapour pressure deficit in hPa
 #' @param rain numeric, hourly rainfall mean in mm
 #'
 #' @return
 #'
 #' @examples
+#' calc_DOR(Tm_h = 22,
+#'          vpd_h = 0.05,
+#'          rain_h = 0)
 calc_DOR <- function(Tm_h, vpd_h, rain_h){
   rain <- vpd <- HT_h <- NULL
 
-  if(rain > 0 | vpd == 4.5){
+  if(rain_h > 0 | vpd_h <= 4.5){
     M_h <- 1
   }else{
-    if(rain == 0 & vpd > 4.5){
+    if(rain_h == 0 & vpd_h > 4.5){
       M_h <- 0
     }
+
+    return(M_h)
   }
 
 
