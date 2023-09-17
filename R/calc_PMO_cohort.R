906e33be334e743bd264000d388b1bc548b37290
PaulMelloy <paul@melloy.com.au>
2023-09-18 08:34:56 +1000

    init COMMENTED function to index germinating oospore cohorts

---
+++ R/calc_PMO_cohort.R
@@ -0,0 +1,24 @@
+#' Calculate the cohort size of phyiologically mature oospores
+#'
+#' @param j rain event number triggering oospore germination
+#' @param epslon hour at which the germination is triggared
+#' @param DOR_h rate at which oospores matiure at hour h
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_PMO_cohort <- function(j, epslon,DOR_h){
+  # mean(DOR_h[j],DOR_h[j-1])
+
+  # # Index oospore cohort
+  # w[, J_cohort := 0] # initiate the variable
+  # for(i in seq_along(w$J_cohort)){
+  #   if(i == 1) next
+  #   c1 <- fifelse(w[i-1,rain] < 0.2 & w[i,rain] >= 0.2,
+  #                 w[i,J_cohort] +1,
+  #                 w[i,J_cohort])
+  #   w[i:.N, J_cohort := c1]
+  #
+  # }
+}
