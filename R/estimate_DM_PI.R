fc8f77553cab1d51906a2fc5ba082a18cc3d7672
PaulMelloy <paul@melloy.com.au>
2023-09-18 08:36:00 +1000

    add germinating oospore function in code Add check for NA rainfall

--- R/estimate_DM_PI.R
+++ R/estimate_DM_PI.R
@@ -67,6 +67,8 @@ estimate_DM_PI <- function(w,
   # add VPD column
   w[, vpd := epiphytoolR::calc_vpd(rh,temp)]
 
+  if(any(is.na(w$rain)))stop("rainfall contains 'NA' values. Please amend")
+
   # Leaf litter moisture sufficient for oospore maturation
   # dichotomic variable
   w[, M_h := data.table::fcase(rain > 0, 1,
@@ -83,6 +85,20 @@ estimate_DM_PI <- function(w,
 
   #calculate physiological maturity
   w[, PMO := MMO * DOR]
+
+  # Index oospore cohort
+  # calc_PMO_cohort()
+  w[, J_cohort := 0] # initiate the variable
+  for(i in seq_along(w$J_cohort)){
+    if(i == 1) next
+    c1 <- fifelse(w[i-1,rain] < 0.2 & w[i,rain] >= 0.2,
+                  w[i,J_cohort] +1,
+                  w[i,J_cohort])
+    w[i:.N, J_cohort := c1]
+
+  }
+
+
 
   return(w)
 
