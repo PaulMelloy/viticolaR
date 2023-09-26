4e864473dd7af56eb11faf5b37b496787820fc8e
PaulMelloy <paul@melloy.com.au>
2023-09-27 09:32:28 +1000

    init tests

---
+++ tests/testthat/test-calc_GEO.R
@@ -0,0 +1,9 @@
+test_that("cumulative survivability GEO is calculated", {
+  sus_v <- calc_SUS(rep(20:35, each = 10),95)
+
+  calc_GEO(SUS_h = sus_v,
+           cohort = rep(c(rep(0,5),1,1,1,
+                      rep(0,2),0,
+                      rep(0,4),0),10)
+           )
+})
---
+++ tests/testthat/test-estimate_DM_PI.R
@@ -0,0 +1,5 @@
+test_that("Testing phase of model", {
+  # library(devtools)
+  library(epiphytoolR)
+  T1 <- estimate_DM_PI(w = nt_weather)
+})
