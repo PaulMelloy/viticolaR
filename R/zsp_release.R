55f648458746d92dd81862b4b9316366dca3d589
PaulMelloy <paul@melloy.com.au>
2023-09-27 09:31:47 +1000

    Init

---
+++ R/zsp_release.R
@@ -0,0 +1,13 @@
+#' Zoospore release
+#'
+#' @param WD_h integer wetness duration in hours
+#' @param TWD_h numeric, mean temperature during wet duration.
+#'
+#' @return logical, TRUE or FALSE
+#'
+#' @examples
+#' zsp_release(4,16.3)
+zsp_release <- function(WD_h,TWD_h){
+  x1 <- exp((-1.022 + 19.634)/TWD_h)
+  return(unique(WD_h) >= x1)
+}
