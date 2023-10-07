492dfca14f6bf0d03513049096dcdcfabe491c6c
PaulMelloy <paul@melloy.com.au>
2023-10-07 18:19:25 +1000

    remove example as not exported

--- R/zsp_release.R
+++ R/zsp_release.R
@@ -4,9 +4,6 @@
 #' @param TWD_h numeric, mean temperature during wet duration.
 #'
 #' @return logical, TRUE or FALSE
-#'
-#' @examples
-#' zsp_release(4,16.3)
 zsp_release <- function(WD_h,TWD_h){
   x1 <- exp((-1.022 + 19.634)/TWD_h)
   return(WD_h >= x1)
