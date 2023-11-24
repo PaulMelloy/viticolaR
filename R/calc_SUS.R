af06ea39ffa6b2b83fed9a3da4660b3e5c8397ae
PaulMelloy <paul@melloy.com.au>
2023-11-24 11:15:59 +0000

    Prevent Inf numbers by removing 100% rh

--- R/calc_SUS.R
+++ R/calc_SUS.R
@@ -6,9 +6,12 @@
 #' @return numeric, a non-dimensional number to indicate the length of sporangia
 #'  survival
 calc_SUS <- function(T_h,RH_h){
+  RH_h <- data.table::fifelse(RH_h == 100,
+                              99.9,
+                              RH_h,
+                              na = NA)
   SUS <- 1/(
-    24*(5.67 * 0.47 *
-          (T_h *(1 - RH_h/100)) +
-          0.01 *(T_h * (1 - RH_h/100))^2))
+    24*(5.67 - 0.47 * (T_h *(1 - (RH_h/100))) +
+          0.01 *(T_h * (1 - (RH_h/100)))^2))
   return(SUS)
 }
